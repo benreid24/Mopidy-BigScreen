fc81e91ecc1aea8450ef44b9d9a7abbdc19c1317
Ben Reid <reidben24@gmail.com>
2024-02-26 23:20:31 -0500

    Expose api endpoint to fetch add_url from backend

--- mopidy_bigscreen/__init__.py
+++ mopidy_bigscreen/__init__.py
@@ -3,10 +3,29 @@ import pathlib
 
 import pkg_resources
 from mopidy import config, ext
+import tornado.web
 
 __version__ = pkg_resources.get_distribution("Mopidy-BigScreen").version
 
 logger = logging.getLogger(__name__)
+
+
+class BigscreenRequestHandler(tornado.web.RequestHandler):
+    def initialize(self, config):
+        cfg = dict(config["bigscreen"])
+        self.result = {
+            'add_url': cfg.get('add_url', '')
+        }
+
+    def get(self):
+        self.write(self.result)
+        self.set_header('Content-Type', 'application/json')
+
+
+def bigscreen_factory(config, core):
+    return [
+        ('/config', BigscreenRequestHandler, {'config': config})
+    ]
 
 
 class Extension(ext.Extension):
@@ -30,3 +49,7 @@ class Extension(ext.Extension):
                 "path": str(pathlib.Path(__file__).parent / "static"),
             },
         )
+        registry.add('http:app', {
+            'name': self.ext_name,
+            'factory': bigscreen_factory,
+        })
